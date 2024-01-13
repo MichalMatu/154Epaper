70a0bf2094781559ce6336a3e6bb33822c335097
MichalMatu <michal.matuszewski1984@gmail.com>
2024-01-13 09:44:45 +0000

    a

--- src/main.cpp
+++ src/main.cpp
@@ -14,60 +14,27 @@ GxEPD2_BW<GxEPD2_154_D67, GxEPD2_154_D67::HEIGHT> display(GxEPD2_154_D67(/*CS=5*
 
 void showPartialUpdate()
 {
-  uint16_t box_x = 10;
-  uint16_t box_y = 15;
-  uint16_t box_w = 70;
-  uint16_t box_h = 20;
-  uint16_t cursor_y = box_y + box_h - 6;
-  if (display.epd2.WIDTH < 104)
-    cursor_y = box_y + 6;
+  uint16_t box_x = 10, box_y = 15, box_w = 70, box_h = 20;
+  uint16_t cursor_y = (display.epd2.WIDTH < 104) ? box_y + 6 : box_y + box_h - 6;
   float value = 13.95;
   uint16_t incr = display.epd2.hasFastPartialUpdate ? 1 : 3;
+
   display.setFont(&FreeMonoBold9pt7b);
   if (display.epd2.WIDTH < 104)
     display.setFont(0);
   display.setTextColor(GxEPD_BLACK);
-  // show where the update box is
-  for (uint16_t r = 0; r < 4; r++)
+
+  display.setRotation(3);
+  display.setPartialWindow(box_x, box_y, box_w, box_h);
+
+  for (uint16_t i = 1; i <= 10; i += incr)
   {
-    display.setRotation(r);
-    display.setPartialWindow(box_x, box_y, box_w, box_h);
-    display.firstPage();
-    do
-    {
-      display.fillRect(box_x, box_y, box_w, box_h, GxEPD_BLACK);
-      // display.fillScreen(GxEPD_BLACK);
-    } while (display.nextPage());
-    delay(2000);
     display.firstPage();
     do
     {
       display.fillRect(box_x, box_y, box_w, box_h, GxEPD_WHITE);
-    } while (display.nextPage());
-    delay(1000);
-  }
-  // return;
-  //  show updates in the update box
-  for (uint16_t r = 0; r < 4; r++)
-  {
-    display.setRotation(r);
-    display.setPartialWindow(box_x, box_y, box_w, box_h);
-    for (uint16_t i = 1; i <= 10; i += incr)
-    {
-      display.firstPage();
-      do
-      {
-        display.fillRect(box_x, box_y, box_w, box_h, GxEPD_WHITE);
-        display.setCursor(box_x, cursor_y);
-        display.print(value * i, 2);
-      } while (display.nextPage());
-      delay(500);
-    }
-    delay(1000);
-    display.firstPage();
-    do
-    {
-      display.fillRect(box_x, box_y, box_w, box_h, GxEPD_WHITE);
+      display.setCursor(box_x, cursor_y);
+      display.print(value * i, 2);
     } while (display.nextPage());
     delay(1000);
   }
