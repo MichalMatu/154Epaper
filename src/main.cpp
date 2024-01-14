9b9d7f802ef7f4188d0cf63e4b266488fe3b845e
MichalMatu <michal.matuszewski1984@gmail.com>
2024-01-14 14:17:18 +0000

    cleaned up

--- src/main.cpp
+++ src/main.cpp
@@ -4,81 +4,39 @@
 #include <GxEPD2_3C.h>
 #include <Fonts/FreeMonoBold12pt7b.h>
 #include "DisplayUpdater.h"
-
 #include "bsec.h"
 #include <Wire.h>
 
-// Helper functions declarations
-void checkIaqSensorStatus(void);
-void errLeds(void);
+// Helper function declarations
+void checkIaqSensorStatus();
+void errLeds();
 
 // Create an object of the class Bsec
 Bsec iaqSensor;
-
 String output;
 
 DisplayUpdater displayUpdater;
 
-// ESP32 CS(SS)=5, SCL(SCK)=18, SDA(MOSI)=23, BUSY=15, RES(RST)=12, DC=14
+// Define BME68X sensor address
+#define BME68X_I2C_ADDR_HIGH 0x77
+
+// Define gas resistance normalization parameters
+const float minResistance = 10000.0;  // Replace with your actual minimum resistance
+const float maxResistance = 500000.0; // Replace with your actual maximum resistance
 
 GxEPD2_BW<GxEPD2_154_D67, GxEPD2_154_D67::HEIGHT> display(GxEPD2_154_D67(5, 14, 12, 15));
 
-// Function to be executed on core 0
-void taskCore0(void *parameter)
-{
-  for (;;)
-  {
-    Serial.println("Task on Core 0");
-    delay(1000);
-  }
-}
-
-// Function to be executed on core 1
-void taskCore1(void *parameter)
-{
-  for (;;)
-  {
-    Serial.println("Task on Core 1");
-    delay(1000);
-  }
-}
-
 void setup()
 {
-  // serial port initialization
   Serial.begin(115200);
   display.init(115200, true, 50, false);
-  displayUpdater.showPartialUpdate(10, 15, 70, 20, 13.95); // Example values
-  displayUpdater.showPartialUpdate(10, 40, 70, 20, 13.95); // Example values
-  display.hibernate();
-
-  // Create and start tasks
-  xTaskCreatePinnedToCore(
-      taskCore0,   // Function to run on core 0
-      "TaskCore0", // Name of the task
-      10000,       // Stack size (bytes)
-      NULL,        // Parameter to pass to the function
-      1,           // Task priority
-      NULL,        // Task handle
-      0);          // Core number (0 or 1)
-
-  xTaskCreatePinnedToCore(
-      taskCore1,   // Function to run on core 1
-      "TaskCore1", // Name of the task
-      10000,       // Stack size (bytes)
-      NULL,        // Parameter to pass to the function
-      1,           // Task priority
-      NULL,        // Task handle
-      1);          // Core number (0 or 1)
 
   Wire.begin();
-  pinMode(LED_BUILTIN, OUTPUT);
   iaqSensor.begin(BME68X_I2C_ADDR_HIGH, Wire);
-  output = "\nBSEC library version " + String(iaqSensor.version.major) + "." + String(iaqSensor.version.minor) + "." + String(iaqSensor.version.major_bugfix) + "." + String(iaqSensor.version.minor_bugfix);
-  Serial.println(output);
+
   checkIaqSensorStatus();
 
-  bsec_virtual_sensor_t sensorList[13] = {
+  bsec_virtual_sensor_t sensorList[] = {
       BSEC_OUTPUT_IAQ,
       BSEC_OUTPUT_STATIC_IAQ,
       BSEC_OUTPUT_CO2_EQUIVALENT,
@@ -93,81 +51,56 @@ void setup()
       BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
       BSEC_OUTPUT_GAS_PERCENTAGE};
 
-  iaqSensor.updateSubscription(sensorList, 13, BSEC_SAMPLE_RATE_LP);
+  iaqSensor.updateSubscription(sensorList, sizeof(sensorList) / sizeof(sensorList[0]), BSEC_SAMPLE_RATE_LP);
   checkIaqSensorStatus();
-
-  // Print the header
-  output = "Timestamp [ms], IAQ, IAQ accuracy, Static IAQ, CO2 equivalent, breath VOC equivalent, raw temp[°C], pressure [hPa], raw relative humidity [%], gas [Ohm], Stab Status, run in status, comp temp[°C], comp humidity [%], gas percentage";
-  Serial.println(output);
 }
 
-void loop(void)
+float normalizeGasResistance(float gasResistance)
 {
-  unsigned long time_trigger = millis();
+  gasResistance = constrain(gasResistance, minResistance, maxResistance);
+  return map(gasResistance, minResistance, maxResistance, 0, 100);
+}
+
+void loop()
+{
+  float normalizedGasResistance = normalizeGasResistance(iaqSensor.gasResistance);
+
   if (iaqSensor.run())
-  { // If new data is available
-    digitalWrite(LED_BUILTIN, LOW);
-    output = String(time_trigger);
-    output += ", " + String(iaqSensor.iaq);
-    output += ", " + String(iaqSensor.iaqAccuracy);
-    output += ", " + String(iaqSensor.staticIaq);
-    output += ", " + String(iaqSensor.co2Equivalent);
-    output += ", " + String(iaqSensor.breathVocEquivalent);
-    output += ", " + String(iaqSensor.rawTemperature);
-    output += ", " + String(iaqSensor.pressure);
-    output += ", " + String(iaqSensor.rawHumidity);
-    output += ", " + String(iaqSensor.gasResistance);
-    output += ", " + String(iaqSensor.stabStatus);
-    output += ", " + String(iaqSensor.runInStatus);
-    output += ", " + String(iaqSensor.temperature);
-    output += ", " + String(iaqSensor.humidity);
-    output += ", " + String(iaqSensor.gasPercentage);
-    Serial.println(output);
-    digitalWrite(LED_BUILTIN, HIGH);
+  {
+    displayUpdater.showPartialUpdate(10, 15, 70, 20, iaqSensor.temperature);
+    displayUpdater.showPartialUpdate(10, 40, 70, 20, iaqSensor.humidity);
+    displayUpdater.showPartialUpdate(10, 65, 70, 20, iaqSensor.pressure);
+    displayUpdater.showPartialUpdate(10, 90, 70, 20, normalizedGasResistance);
   }
   else
   {
     checkIaqSensorStatus();
   }
+
+  delay(5000);
 }
 
 // Helper function definitions
-void checkIaqSensorStatus(void)
+void checkIaqSensorStatus()
 {
   if (iaqSensor.bsecStatus != BSEC_OK)
   {
-    if (iaqSensor.bsecStatus < BSEC_OK)
-    {
-      output = "BSEC error code : " + String(iaqSensor.bsecStatus);
-      Serial.println(output);
-      for (;;)
-        errLeds(); /* Halt in case of failure */
-    }
-    else
-    {
-      output = "BSEC warning code : " + String(iaqSensor.bsecStatus);
-      Serial.println(output);
-    }
+    output = "BSEC error code : " + String(iaqSensor.bsecStatus);
+    Serial.println(output);
+    for (;;)
+      errLeds(); /* Halt in case of failure */
   }
 
   if (iaqSensor.bme68xStatus != BME68X_OK)
   {
-    if (iaqSensor.bme68xStatus < BME68X_OK)
-    {
-      output = "BME68X error code : " + String(iaqSensor.bme68xStatus);
-      Serial.println(output);
-      for (;;)
-        errLeds(); /* Halt in case of failure */
-    }
-    else
-    {
-      output = "BME68X warning code : " + String(iaqSensor.bme68xStatus);
-      Serial.println(output);
-    }
+    output = "BME68X error code : " + String(iaqSensor.bme68xStatus);
+    Serial.println(output);
+    for (;;)
+      errLeds(); /* Halt in case of failure */
   }
 }
 
-void errLeds(void)
+void errLeds()
 {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
