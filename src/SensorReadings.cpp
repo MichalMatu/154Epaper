77e47c6aa8f252b07d4b25e8b9fd49d63cdb98bb
MichalMatu <michal.matuszewski1984@gmail.com>
2024-01-17 11:56:44 +0000

    a

--- src/SensorReadings.cpp
+++ src/SensorReadings.cpp
@@ -14,24 +14,12 @@ float SensorReadings::getTemperature()
 {
     sensors_event_t humidity, temp;
     aht.getEvent(&humidity, &temp); // populate temp and humidity objects with fresh data
-    Serial.print("Temperature: ");
-    Serial.print(temp.temperature);
-    Serial.println(" degrees C");
-    Serial.print("Humidity: ");
-    Serial.print(humidity.relative_humidity);
-    Serial.println("% rH");
-    return temp.temperature; // Return the temperature value
+    return temp.temperature;        // Return the temperature value
 }
 
 float SensorReadings::getHumidity()
 {
     sensors_event_t humidity, temp;
-    aht.getEvent(&humidity, &temp); // populate temp and humidity objects with fresh data
-    Serial.print("Temperature: ");
-    Serial.print(temp.temperature);
-    Serial.println(" degrees C");
-    Serial.print("Humidity: ");
-    Serial.print(humidity.relative_humidity);
-    Serial.println("% rH");
+    aht.getEvent(&humidity, &temp);    // populate temp and humidity objects with fresh data
     return humidity.relative_humidity; // Return the humidity value
 }
