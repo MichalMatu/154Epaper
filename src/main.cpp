02552c47a6ee0146b9d8410a8ad0d9cf8b590832
MichalMatu <michal.matuszewski1984@gmail.com>
2024-01-17 13:18:31 +0000

    before sgp41

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,8 @@
 #include <Adafruit_AHTX0.h>
 
 #include "DisplayUpdater.h"
-#include "SensorReadings.h"
 
 DisplayUpdater displayUpdater;
-
-Adafruit_AHTX0 aht;
-SensorReadings sensorReadings(aht);
 
 // ESP32 CS(SS)=5, SCL(SCK)=18, SDA(MOSI)=23, BUSY=15, RES(RST)=12, DC=14
 
@@ -22,13 +18,6 @@ void setup()
 
 {
   Serial.begin(115200);
-  if (!aht.begin())
-  {
-    Serial.println("Could not find AHT? Check wiring");
-    while (1)
-      delay(10);
-  }
-  Serial.println("AHT10 or AHT20 found");
 
   display.init(115200, true, 50, false);
 }
@@ -36,8 +25,8 @@ void setup()
 void loop()
 {
 
-  float temperature = sensorReadings.getTemperature();
-  float humidity = sensorReadings.getHumidity();
+  float temperature = 0;
+  float humidity = 0;
   displayUpdater.showPartialUpdate(10, 15, 70, 20, temperature);
   displayUpdater.showPartialUpdate(10, 40, 70, 20, humidity);
 
